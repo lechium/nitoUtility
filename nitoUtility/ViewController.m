e97e217a3b469df13156c0d38bc161aecb005f54
Kevin Bradley <kevin.w.bradley@me.com>
2019-09-18 22:18:39 -0700

    ready for next release 1.1

--- nitoUtility/ViewController.m
+++ nitoUtility/ViewController.m
@@ -73,71 +73,19 @@
     });
 }
 
-- (void)showInvalidPasswordAlert:(SEL)selector value:(id)value {
-    
-    UIAlertController *alertCon = [UIAlertController alertControllerWithTitle:@"Error authenticating" message:@"Please enter the root password for your AppleTV" preferredStyle:UIAlertControllerStyleAlert];
-    [alertCon addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
-        textField.secureTextEntry = true;
-    }];
-    UIAlertAction *setPassword = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        
-        NSString *newPassword = alertCon.textFields[0].text;
-        NSError *newError = nil;
-        SSHWrapper *wrapper = [SSHWrapper new];[wrapper connectToHost:self.device.ipAddress port:22 user:@"root" password:newPassword error:&newError];
-        
-        if (newError == nil){
-            [SVProgressHUD show];
-            UICKeyChainStore *store = [UICKeyChainStore keyChainStoreWithService:self.device.serviceName];
-            store[@"password"] = newPassword;
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-                [self performSelector:selector withObject:value];
-                //[self fixStuffOnSession:wrapper];
-            });
-            
-        } else {
-            [self showInvalidPasswordAlert:selector value:value];
-        }
-    }];
-    [alertCon addAction:setPassword];
-    [alertCon addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self presentViewController:alertCon animated:true completion:nil];
-    });
-}
-
-
 - (void)fixCurrentDevice {
-    if (![self isJailbroken]){
-        //NSLog(@"is NOT jailbroken!");
-        [self showAlertWithTitle:@"An error occured" message:[NSString stringWithFormat:@"The %@ '%@' running %@ is not currently jailbroken.", self.device.serviceDictionary[@"model"], self.device.serviceName, self.device.serviceDictionary[@"osvers"]]];
-        return;
-    }
-    __block NSError *connectError = nil;
-    if (self.session == nil){
-        self.session = [SSHWrapper new];
-        UICKeyChainStore *store = [UICKeyChainStore keyChainStoreWithService:self.device.serviceName];
-        NSString *pwCheck = store[@"password"];
-        if (!pwCheck){
-            pwCheck = @"alpine";
-        }
-        [self.session connectToHost:self.device.ipAddress port:22 user:@"root" password:pwCheck error:&connectError];
-        if (connectError){
-            NSLog(@"connection error: %@", connectError);
-            [self showInvalidPasswordAlert:@selector(fixStuffOnSession:) value:self.session];
-        } else {
+    
+    [self createSessionWithBlock:^(BOOL success) {
+        
+        if (success){
             [SVProgressHUD show];
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-                [self fixStuffOnSession:self.session];
+                [self fixStuffOnSession];
+                [self populateApplications];
             });
         }
-    }  else {
-        [SVProgressHUD show];
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-            [self fixStuffOnSession:self.session];
-        });
-    }
-    
-    
+        
+    }];
     
 }
 
@@ -166,7 +114,7 @@
 }
 
 
-- (void)fixStuffOnSession:(SSHWrapper *)wrapper {
+- (void)fixStuffOnSession {
     
     __block NSError *connectError = nil;
     NSInteger issuesDetected = 0;
@@ -174,19 +122,19 @@
     NSString *csCheckCommand = @"/usr/local/bin/jtool --sig /usr/lib/libssl.1.1.dylib";
     NSString *tokenCheckCommand = @"file /var/mobile/Documents/.token";
     NSString *permissionCheckCommand = @"/usr/bin/stat -c \"%a\" /usr/libexec/goNito";
-    NSString *sslCodeSignCheck = [wrapper executeCommand:csCheckCommand error:&connectError];
-    NSString *goNitoPermissionCheck = [wrapper executeCommand:permissionCheckCommand error:nil];
-    NSString *tokenFileCheck = [wrapper executeCommand:tokenCheckCommand error:nil];
+    NSString *sslCodeSignCheck = [self.session executeCommand:csCheckCommand error:&connectError];
+    NSString *goNitoPermissionCheck = [self.session executeCommand:permissionCheckCommand error:nil];
+    NSString *tokenFileCheck = [self.session executeCommand:tokenCheckCommand error:nil];
     NSString *keyCheckCommand = @"/usr/bin/bash apt-key list | grep -c \"030F 5D6E 7E14 4939 7E04  B6B2 5330 AE38 84B9 841D\"";
     NSMutableString *fixedIssues = [NSMutableString new];
-    NSString *keyCheck = [wrapper executeCommand:keyCheckCommand error:nil];
+    NSString *keyCheck = [self.session executeCommand:keyCheckCommand error:nil];
     if (keyCheck.integerValue != 1){
         issuesDetected++;
         NSString *keyCommand = @"/usr/bin/bash /usr/bin/apt-key add /Applications/nitoTV.app/pub.key";
-        [wrapper executeCommand:keyCommand error:nil];
+        [self.session executeCommand:keyCommand error:nil];
         keyCommand = @"/usr/bin/bash /usr/bin/apt-key add /Applications/nitoTV.app/pub2.key";
-        [wrapper executeCommand:keyCommand error:nil];
-        keyCheck = [wrapper executeCommand:keyCheckCommand error:nil];
+        [self.session executeCommand:keyCommand error:nil];
+        keyCheck = [self.session executeCommand:keyCheckCommand error:nil];
         if (keyCheck.integerValue == 1){
             issuesFixed++;
             NSLog(@"missing keys fixed!");
@@ -196,8 +144,8 @@
     if ([goNitoPermissionCheck integerValue] != 6755){
         issuesDetected++;
         NSLog(@"invalid permissions detected for goNito, repairing...");
-        [wrapper executeCommand:@"/usr/bin/chmod 6755 /usr/libexec/goNito" error:nil];
-        goNitoPermissionCheck =  [wrapper executeCommand:permissionCheckCommand error:nil];
+        [self.session executeCommand:@"/usr/bin/chmod 6755 /usr/libexec/goNito" error:nil];
+        goNitoPermissionCheck =  [self.session executeCommand:permissionCheckCommand error:nil];
         if (goNitoPermissionCheck.integerValue == 6755){
             issuesFixed++;
             NSString *currentIssue = @"goNito permissions fixed!\n";
@@ -209,8 +157,8 @@
     if (![sslCodeSignCheck containsString:@"CDHash"]){
         issuesDetected++;
         NSLog(@"Detected unsigned libssl, Repairing!");
-        [wrapper executeCommand:@"/usr/local/bin/jtool --sign platform --inplace /usr/lib/libssl.1.1.dylib" error:nil];
-        sslCodeSignCheck = [wrapper executeCommand:csCheckCommand error:&connectError];
+        [self.session executeCommand:@"/usr/local/bin/jtool --sign platform --inplace /usr/lib/libssl.1.1.dylib" error:nil];
+        sslCodeSignCheck = [self.session executeCommand:csCheckCommand error:&connectError];
         if ([sslCodeSignCheck containsString:@"CDHash"]){
             issuesFixed++;
             NSString *currentIssue = @"unsigned libssl.1.1.dylib fixed!\n";
@@ -223,8 +171,8 @@
         
         issuesDetected++;
         NSLog(@"Missing hidden token, this file is a relic but if its missing it can cause crashes in older versions preventing updates. Repairing!");
-        [wrapper executeCommand:@"/usr/bin/touch /var/mobile/Documents/.token" error:nil];
-        tokenFileCheck = [wrapper executeCommand:tokenCheckCommand error:nil];
+        [self.session executeCommand:@"/usr/bin/touch /var/mobile/Documents/.token" error:nil];
+        tokenFileCheck = [self.session executeCommand:tokenCheckCommand error:nil];
         if (![tokenFileCheck containsString:@"No such file or directory"]){
             issuesFixed++;
             NSString *currentIssue = @"Missing .token file fixed!\n";
@@ -234,45 +182,45 @@
     }
     NSString *checkAptCommand = @"/usr/bin/apt-get check > /var/root/aptout 2&> /var/root/aptout";
     NSString *fixCommand = @"apt-get -f install";
-    [wrapper executeCommand:checkAptCommand error:nil];
+    [self.session executeCommand:checkAptCommand error:nil];
     NSError *theError = nil;
-    NSString *checkApt = [wrapper executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
-    [wrapper executeCommand:@"/usr/bin/rm /var/root/aptout" error:nil];
+    NSString *checkApt = [self.session executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
+    [self.session executeCommand:@"/usr/bin/rm /var/root/aptout" error:nil];
     NSString *interruptedDPKG = @"E: dpkg was interrupted, you must manually run 'dpkg --configure -a' to correct the problem.";
     if ([checkApt containsString:fixCommand]){
         NSLog(@"found unmet dependencies issue!");
         issuesDetected++;
-        [wrapper executeCommand:[fixCommand stringByAppendingString:@" -y --force-yes"] error:nil];
-        [wrapper executeCommand:checkAptCommand error:nil];
+        [self.session executeCommand:[fixCommand stringByAppendingString:@" -y --force-yes"] error:nil];
+        [self.session executeCommand:checkAptCommand error:nil];
         sleep(2);
         //NSString *ls = [wrapper executeCommand:@"ls -al /var/root/aptout" error:nil];
         //NSLog(@"ls: %@", ls);
-        NSString *newCheckApt = [wrapper executeCommand:@"/usr/bin/cat /var/root/aptout" error:nil];
+        NSString *newCheckApt = [self.session executeCommand:@"/usr/bin/cat /var/root/aptout" error:nil];
         NSLog(@"newCheckApt: %@", newCheckApt);
-        newCheckApt = [wrapper executeCommand:@"/usr/bin/cat /var/root/aptout" error:nil];
+        newCheckApt = [self.session executeCommand:@"/usr/bin/cat /var/root/aptout" error:nil];
         NSLog(@"newCheckApt: %@", newCheckApt);
         if (![newCheckApt containsString:fixCommand]){
             issuesFixed++;
             NSString *currentIssue = @"Unmet dependency issue fixed!\n";
             NSLog(@"%@", currentIssue);
             [fixedIssues appendString:currentIssue];
-            [wrapper executeCommand:@"/usr/bin/rm aptout" error:nil];
+            [self.session executeCommand:@"/usr/bin/rm aptout" error:nil];
             
         } else {
-            [wrapper executeCommand:@"/usr/bin/rm aptout" error:nil];
+            [self.session executeCommand:@"/usr/bin/rm aptout" error:nil];
             
             //try try again
             NSString *newCheck = @"/usr/bin/apt-get check /dev/null | grep bulletinh4x -c -m 1";
-            NSString *h4xCheck = [wrapper executeCommand:newCheck error:nil];
+            NSString *h4xCheck = [self.session executeCommand:newCheck error:nil];
             NSLog(@"h4xCheck: %@", h4xCheck);
             // if (h4xCheck == 1){
-            [wrapper executeCommand:@"dpkg -r com.matchstic.reprovision" error:nil];
-            [wrapper executeCommand:@"apt-get -f install -y --force-yes" error:nil];
-            [wrapper executeCommand:@"apt-get install com.matchstic.reprovision=0.4.5 -y --force-yes" error:nil];
-            [wrapper executeCommand:checkAptCommand error:nil];
-            checkApt = [wrapper executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
-            checkApt = [wrapper executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
-            [wrapper executeCommand:@"/usr/bin/rm /var/root/aptout" error:nil];
+            [self.session executeCommand:@"dpkg -r com.matchstic.reprovision" error:nil];
+            [self.session executeCommand:@"apt-get -f install -y --force-yes" error:nil];
+            [self.session executeCommand:@"apt-get install com.matchstic.reprovision=0.4.5 -y --force-yes" error:nil];
+            [self.session executeCommand:checkAptCommand error:nil];
+            checkApt = [self.session executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
+            checkApt = [self.session executeCommand:@"/usr/bin/cat /var/root/aptout" error:&theError];
+            [self.session executeCommand:@"/usr/bin/rm /var/root/aptout" error:nil];
             if (![checkApt containsString:fixCommand]){
                 issuesFixed++;
                 NSString *currentIssue = @"Reprovision dependency issue fixed!\n";
@@ -284,11 +232,11 @@
     } else if ([checkApt containsString:interruptedDPKG]){
         issuesDetected++;
         NSError *myErrors = nil;
-        NSString *config = [wrapper executeCommand:@"/usr/bin/dpkg --configure -a" error:&myErrors];
+        NSString *config = [self.session executeCommand:@"/usr/bin/dpkg --configure -a" error:&myErrors];
         //NSLog(@"config: %@", config);
-        [wrapper executeCommand:checkAptCommand error:nil];
-        checkApt = [wrapper executeCommand:@"/usr/bin/cat aptout" error:&theError];
-        [wrapper executeCommand:@"/usr/bin/rm aptout" error:nil];
+        [self.session executeCommand:checkAptCommand error:nil];
+        checkApt = [self.session executeCommand:@"/usr/bin/cat aptout" error:&theError];
+        [self.session executeCommand:@"/usr/bin/rm aptout" error:nil];
         if (![checkApt containsString:interruptedDPKG]){
             issuesFixed++;
             NSString *currentIssue = @"Unmet dependency issue fixed!\n";
@@ -296,17 +244,17 @@
             [fixedIssues appendString:currentIssue];
         } else {
             NSString *dpkgFirstCheck = @"dpkg -C | grep -c \"half configured\"";
-            NSInteger returnValue = [[wrapper executeCommand:dpkgFirstCheck error:nil] integerValue];
+            NSInteger returnValue = [[self.session executeCommand:dpkgFirstCheck error:nil] integerValue];
             if (returnValue == 1){
                 NSString *dpkgStatus =  @"dpkg -C | grep -m 1 \"appletvos\" | cut -d \":\" -f 1 | tr -d \" \"";
-                NSString *errantPackage = [wrapper executeCommand:dpkgStatus error:nil];
+                NSString *errantPackage = [self.session executeCommand:dpkgStatus error:nil];
                 NSLog(@"errant package: %@", errantPackage);
                 NSString *rmCmd = [NSString stringWithFormat:@"dpkg -r %@", errantPackage];
-                [wrapper executeCommand:rmCmd error:nil];
-                [wrapper executeCommand:@"/usr/bin/dpkg --configure -a" error:&myErrors];
-                [wrapper executeCommand:checkAptCommand error:nil];
-                checkApt = [wrapper executeCommand:@"/usr/bin/cat aptout" error:&theError];
-                [wrapper executeCommand:@"/usr/bin/rm aptout" error:nil];
+                [self.session executeCommand:rmCmd error:nil];
+                [self.session executeCommand:@"/usr/bin/dpkg --configure -a" error:&myErrors];
+                [self.session executeCommand:checkAptCommand error:nil];
+                checkApt = [self.session executeCommand:@"/usr/bin/cat aptout" error:&theError];
+                [self.session executeCommand:@"/usr/bin/rm aptout" error:nil];
                 if (![checkApt containsString:interruptedDPKG]){
                     issuesFixed++;
                     NSString *currentIssue = @"Interrupted dpkg issue fixed!\n";
@@ -686,7 +634,10 @@
     if (self.session != nil){
         if (self.applications == nil){
             self.applications = [self availableApps];
-            [self.tableView reloadData];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                 [self.tableView reloadData];
+            });
+           
         }
     }
     
